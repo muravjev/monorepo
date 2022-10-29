a6046bad48dbfa5e0a6e3af12831399042a0760e
ms <sergey.muravjev@gmail.com>
2022-10-29 14:57:02 +0300

    chore(foo): setup ts-next

--- apps/foo/next.config.js
+++ apps/foo/next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-    swcMinify: true
+    swcMinify: true,
+    experimental: {
+        externalDir: true //< for workspaces
+    }
 };
 
 module.exports = nextConfig;
